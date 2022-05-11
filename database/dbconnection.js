a638348d6ae0e90b84d6a644454d78393382da5a
Zuijd <dca.vanzuijdam@student.avans.nl>
2022-05-11 14:27:14 +0200

    dbconnection fix

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -9,8 +9,20 @@ const pool = mysql.createPool({
     database: process.env.DB_DATABASE,
 });
 
+pool.on('acquire', function (connection) {
+    console.log('Connection %d acquired', connection.threadId);
+});
+
 pool.on('connection', function (connection) {
     connection.query('SET SESSION auto_increment_increment=1')
+});
+
+pool.on('enqueue', function () {
+    console.log('Waiting for available connection slot');
+});
+
+pool.on('release', function (connection) {
+    console.log('Connection %d released', connection.threadId);
 });
 
 module.exports = pool;
