5bdd6eb9732e4da4253d448bf59e63a553b1cc23
Zuijd <dca.vanzuijdam@student.avans.nl>
2022-05-11 14:12:50 +0200

    user.controller.test restored

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -1,283 +1,209 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../../index')
+
 const assert = require('assert')
-const dbconnection = require('../../database/dbconnection')
-
-let controller = {
-    validateUser: (req, res, next) => {
-        let user = req.body;
-        let {
-            firstName,
-            lastName,
-            street,
-            city,
-            password,
-            emailAdress
-        } = user;
-        try {
-            assert(typeof firstName === 'string', 'Firstname must be a string')
-            assert(typeof lastName === 'string', 'Lastname must be a string')
-            assert(typeof street === 'string', 'Street must be a string')
-            assert(typeof city === 'string', 'City must be a string')
-            assert(typeof password === 'string', 'Password must be a string')
-            assert(typeof emailAdress === 'string', 'EmailAdress must be a string')
-            next();
-        } catch (err) {
-            const error = {
-                status: 400,
-                message: err.message,
-            };
-
-
-            next(error)
-        }
-
-    },
-
-    checkMail: (req, res, next) => {
-        let user = req.body;
-        let {
-            emailAdress
-        } = user;
-        try {
-            assert(typeof emailAdress === 'string', 'EmailAdress must be a string')
-            next();
-        } catch (err) {
-            const error = {
-                status: 400,
-                message: err.message,
-            };
-
-
-            next(error)
-        }
-
-    },
-
-    addUser: (req, res) => {
-
-        dbconnection.getConnection(function (err, connection) {
-            if (err) throw err;
-
-            let user = req.body;
-
-            connection.query(
-                'SELECT COUNT(emailAdress) as count FROM user WHERE emailAdress = ?',
-                user.emailAdress,
-                function (error, results, fields) {
-
-                    if (error) throw error;
-
-                    if (results[0].count > 0) {
-                        res.status(409).json({
-                            status: 409,
-                            message: "User already exist",
-                        });
-                    } else {
-                        connection.query(
-                            `INSERT INTO user (firstName, lastName, street, city, password, emailAdress) VALUES ('${user.firstName}', '${user.lastName}', '${user.street}', '${user.city}', '${user.password}', '${user.emailAdress}')`,
-                            function (error, results, fields) {
-                                connection.release();
-
-                                if (error) throw error;
-
-                                user = {
-                                    "id": results.insertId,
-                                    ...user,
-                                }
-
-                                if (results.affectedRows > 0) {
-                                    res.status(201).json({
-                                        status: 201,
-                                        result: user,
-                                    });
-                                }
-                            });
-                    }
-                });
-
-
-
-        });
-    },
-
-    getAllUsers: (req, res) => {
-        dbconnection.getConnection(function (err, connection) {
-            if (err) throw err;
-
-            connection.query(
-                'SELECT * FROM user',
-                function (error, results, fields) {
-                    connection.release();
-
-                    if (error) throw error;
-
-                    console.log('#results = ', results.length);
-                    res.status(200).json({
-                        status: 200,
-                        result: results,
-                    });
-                });
-        });
-    },
-
-    getUserByid: (req, res) => {
-
-        dbconnection.getConnection(function (err, connection) {
-            if (err) throw err;
-
-            const userId = req.params.userId;
-
-            connection.query(
-                'SELECT COUNT(id) as count FROM user WHERE id = ?',
-                userId,
-                function (error, results, fields) {
-
-                    if (error) throw error;
-
-
-                    if (results[0].count === 0) {
-                        res.status(404).json({
-                            status: 404,
-                            message: "This user does not exist",
-                        });
-                    } else {
-                        connection.query(
-                            'SELECT * FROM `user` WHERE `id` = ' + userId + '',
-                            function (error, results, fields) {
-                                connection.release();
-
-                                if (error) throw error;
-
-                                console.log('#results = ', results.length);
-                                res.status(200).json({
-                                    status: 200,
-                                    result: results[0],
-                                });
-                            });
-                    }
-                });
-
-
-
-
-        });
-    },
-
-    getUserProfile: (req, res) => {
-        res.status(401).json({
-            status: 401,
-            message: "This functionality has not been realised (yet)!",
-        });
-    },
-
-    updateUser: (req, res, next) => {
-
-        dbconnection.getConnection(function (err, connection) {
-            if (err) throw err;
-
-            let user = req.body;
-            const userId = req.params.userId;
-
-            connection.query(
-                `SELECT COUNT(id) as count FROM user WHERE id = ?`,
-                userId,
-                function (error, results, fields) {
-
-                    if (error) throw error;
-
-                    if (results[0].count === 0) {
-                        res.status(400).json({
-                            status: 400,
-                            message: "This user does not exist",
-                        });
-                    } else {
-                        connection.query(
-                            `SELECT COUNT(emailAdress) as count FROM user WHERE emailAdress = ? AND NOT id = '${userId}'`,
-                            user.emailAdress,
-                            function (error, results, fields) {
-
-                                if (error) throw error;
-
-                                if (results[0].count > 0) {
-                                    res.status(409).json({
-                                        status: 409,
-                                        message: "This email is alreday taken",
-                                    });
-                                } else {
-                                    connection.query(
-                                        'SELECT * FROM `user` WHERE `id` = ' + userId + '',
-                                        function (error, results, fields) {
-
-                                            if (error) throw error;
-
-                                            let newUser = {
-                                                ...results[0],
-                                                ...user,
-                                            }
-
-                                            connection.query(
-                                                `UPDATE user SET firstname = '${newUser.firstName}', lastname = '${newUser.lastName}', street = '${newUser.street}', city = '${newUser.city}', password = '${newUser.password}', emailAdress = '${newUser.emailAdress}', phoneNumber = '${newUser.phoneNumber}', isActive = '${newUser.isActive}' WHERE id = '${userId}'`,
-                                                function (error, results, fields) {
-                                                    connection.release();
-
-                                                    if (error) throw error;
-
-                                                    console.log('#results = ', results.length);
-                                                    res.status(200).json({
-                                                        status: 200,
-                                                        result: newUser,
-                                                    });
-                                                });
-                                        });
-                                }
-                            });
-
-                    }
-                });
-
-        });
-    },
-
-    deleteUser: (req, res) => {
-
-        dbconnection.getConnection(function (err, connection) {
-            if (err) throw err;
-
-            const userId = req.params.userId;
-
-            connection.query(
-                'SELECT COUNT(id) as count FROM user WHERE id = ?',
-                userId,
-                function (error, results, fields) {
-
-                    if (error) throw error;
-
-                    if (results[0].count === 0) {
-                        res.status(400).json({
-                            status: 400,
-                            message: "User does not exist",
-                        });
-                    } else {
-                        connection.query(
-                            `DELETE FROM user WHERE id = '${userId}'`,
-                            function (error, results, fields) {
-                                connection.release();
-
-                                if (error) throw error;
-
-                                console.log('#results = ', results.length);
-                                res.status(200).json({
-                                    status: 200,
-                                    message: "User succesfully deleted",
-                                });
-                            });
-                    }
-                });
-
-
-
-        });
-    },
-
-}
-
-module.exports = controller
+chai.should()
+chai.expect()
+chai.use(chaiHttp)
+
+let createdUserId;
+
+describe('UC-2 Manage users /api/user', () => {
+    describe('UC-201 Register as new user', () => {
+
+        it('TC-201-1 Mandatory field is missing', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: "Chai",
+                    lastName: "Test",
+                    city: "City",
+                    emailAdress: "chai@test.com",
+                    password: "pass123",
+                })
+                .end((err, res) => {
+                    assert.ifError(err)
+                    res.should.have.status(400)
+                    res.should.be.an('object')
+
+                    res.body.should.be
+                        .an('object')
+                        .that.has.all.keys('status', 'message')
+
+                    let {
+                        status,
+                        message
+                    } = res.body
+                    status.should.be.a('number')
+                    message.should.be
+                        .a('string')
+                        .that.contains('Street must be a string')
+
+                    done()
+                })
+        })
+
+        it('TC-201-2 Invalid email address', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: "Chai",
+                    lastName: "Test",
+                    street: "street",
+                    city: "City",
+                    emailAdress: 1,
+                    password: "pass123",
+                })
+                .end((err, res) => {
+                    assert.ifError(err)
+                    res.should.have.status(400)
+                    res.should.be.an('object')
+
+                    res.body.should.be
+                        .an('object')
+                        .that.has.all.keys('status', 'message')
+
+                    let {
+                        status,
+                        message
+                    } = res.body
+                    status.should.be.a('number')
+                    message.should.be
+                        .a('string')
+                        .that.contains('EmailAdress must be a string')
+
+                    done()
+                })
+        })
+
+        it('TC-201-3 Invalid password', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: "Chai",
+                    lastName: "Test",
+                    street: "street",
+                    city: "City",
+                    emailAdress: "chai@test.com",
+                    password: 1,
+                })
+                .end((err, res) => {
+                    assert.ifError(err)
+                    res.should.have.status(400)
+                    res.should.be.an('object')
+
+                    res.body.should.be
+                        .an('object')
+                        .that.has.all.keys('status', 'message')
+
+                    let {
+                        status,
+                        message
+                    } = res.body
+                    status.should.be.a('number')
+                    message.should.be
+                        .a('string')
+                        .that.contains('Password must be a string')
+
+                    done()
+                })
+        })
+
+        it('TC-201-4 User already exists', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: "Chai",
+                    lastName: "Test",
+                    street: "street",
+                    city: "City",
+                    emailAdress: "chai@test.com",
+                    password: "1",
+                })
+                .end((err, res) => {
+                    assert.ifError(err)
+                    res.should.have.status(409)
+                    res.should.be.an('object')
+
+                    res.body.should.be
+                        .an('object')
+                        .that.has.all.keys('status', 'message')
+
+                    let {
+                        status,
+                        message
+                    } = res.body
+                    status.should.be.a('number')
+                    message.should.be
+                        .a('string')
+                        .that.contains('User already exist')
+
+                    done()
+                })
+        })
+
+        it('TC-201-5 User registered successfully', (done) => {
+            chai.request(server)
+                .post('/api/user')
+                .send({
+                    firstName: "New",
+                    lastName: "User",
+                    street: "Street",
+                    city: "City",
+                    emailAdress: "new@User.com",
+                    password: "newUser123",
+                })
+                .end((err, res) => {
+                    assert.ifError(err)
+                    res.should.have.status(201)
+                    res.should.be.an('object')
+
+
+
+                    res.body.should.be
+                        .an('object')
+                        .that.has.all.keys('status', 'result')
+
+                    let {
+                        status,
+                        result
+                    } = res.body
+                    status.should.be.a('number')
+                    result.should.have.property('firstName').and.to.be.a('string')
+                    result.should.have.property('lastName').and.to.be.a('string')
+                    result.should.have.property('street').and.to.be.a('string')
+                    result.should.have.property('city').and.to.be.a('string')
+                    result.should.have.property('emailAdress').and.to.be.a('string')
+                    result.should.have.property('password').and.to.be.a('string')
+                    createdUserId = result.id
+                    done()
+                })
+        })
+    })
+
+    // describe('UC-206 Delete user', () => {
+    //     it('TC-206-4 User successfully deleted', (done) => {
+    //         chai.request(server)
+    //             .delete('/api/user/' + createdUserId)
+    //             .end((err, res) => {
+    //                 assert.ifError(err)
+    //                 res.should.have.status(200)
+    //                 res.should.be.an('object')
+
+    //                 res.body.should.be
+    //                     .an('object')
+    //                     .that.has.all.keys('status', 'message')
+
+    //                 let {
+    //                     status, 
+    //                     message
+    //                 } = res.body
+    //                 status.should.be.a('number')
+    //                 message.should.be.a('string')
+    //                 .that.contains('User succesfully deleted')
+    //                 done()
+    //             })
+    //     })
+    // })
+})
